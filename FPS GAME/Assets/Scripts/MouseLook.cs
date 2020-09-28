9ce0c9f5f150eed2160829faf1cb7adc184797f3
Kulachi <thaparkulachi@gmail.com>
2020-09-28 17:27:52 +0530

    Final game

---
+++ FPS GAME/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSens = 100f;
+    public Transform playerBody;
+    float xRotataion = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSens;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSens;
+
+        xRotataion -= mouseY;
+        xRotataion = Mathf.Clamp(xRotataion, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotataion, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
